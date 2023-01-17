6b920eef6a0df73a79980bc2a40cdaf752beb9af
Shashikant-15 <shashikant.tanti@knoldus.com>
2023-01-18 01:25:10 +0530

    initial commit

---
+++ src/main/java/com/knoldus/example/springbootactuator/SpringbootActuatorApplication.java
@@ -0,0 +1,17 @@
+package com.knoldus.example.springbootactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorApplication {
+
+	/**
+	 * Main Implementation method
+	 * @param args arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActuatorApplication.class, args);
+	}
+
+}
